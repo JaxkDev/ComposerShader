06af89649dbd414bf3e6637574202360eb333463
JaxkDev <JaxkDev@gmail.com>
2021-04-16 16:01:45 +0100

    Initial commit.

---
+++ .idea/fileTemplates/includes/PHP File Header.php
@@ -0,0 +1,10 @@
+/*
+ * ComposerShader, Experimental shading utility.
+ *
+ * Licensed under the Open Software License version 3.0 (OSL-3.0)
+ * Copyright (C) ${YEAR} JaxkDev
+ *
+ * Twitter :: @JaxkDev
+ * Discord :: JaxkDev#2698
+ * Email   :: JaxkDev@gmail.com
+ */
